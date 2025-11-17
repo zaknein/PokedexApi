e2719cf3cb09ffe47290c0158fed6042422c48b8
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-17 20:13:08 -0300

    initial commit

---
+++ src/test/java/com/zaknein/PokedexApi/PokedexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaknein.PokedexApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
