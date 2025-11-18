2b5754750ea3a41a567a44639a4f65203defa732
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-18 10:29:08 -0300

    postmapping endpoint (empty)

--- src/main/java/com/zaknein/PokedexApi/PokemonController.java
+++ src/main/java/com/zaknein/PokedexApi/PokemonController.java
@@ -3,6 +3,8 @@ package com.zaknein.PokedexApi;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -25,6 +27,11 @@ public class PokemonController{
     @GetMapping("/pokemon/id}")
     public String getPokemonById(@PathVariable int id){
         /* GET POKEMON BY ID */
+    }
+    
+    @PostMapping("/pokemon")
+    public Pokemon createPokemon(@RequestBody PokemonCreater pokemonCreater){
+        /*POST POKEMON */
     }
 
 
