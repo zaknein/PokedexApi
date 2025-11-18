c8f160236ce241899c86b48c201896509b3ae5bd
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-18 19:29:56 -0300

    getters/setters

--- src/main/java/com/zaknein/PokedexApi/PokemonController.java
+++ src/main/java/com/zaknein/PokedexApi/PokemonController.java
@@ -1,10 +1,12 @@
 package com.zaknein.PokedexApi;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
--- src/main/java/com/zaknein/PokedexApi/PokemonCreater.java
+++ src/main/java/com/zaknein/PokedexApi/PokemonCreater.java
@@ -1,31 +1,86 @@
 package com.zaknein.PokedexApi;
 
-
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-
-public class PokemonCreater{
+public class PokemonCreater {
 
     String name;
     String species;
     Double height;
     Double weight;
     String description;
-    LocalDateTime  createdAt;    
+    LocalDateTime createdAt;
     ArrayList<String> types;
 
+    public PokemonCreater() {
+    }
 
-    public PokemonCreater(){}
-
-    public PokemonCreater (String name, String species, Double height, Double weight, String description, LocalDateTime createdAt, 
-    ArrayList<String> types){
-        this.id = id;
+    public PokemonCreater(String name, String species, Double height, Double weight, String description,
+            LocalDateTime createdAt,
+            ArrayList<String> types) {
         this.name = name;
         this.species = species;
         this.height = height;
         this.weight = weight;
         this.description = description;
         this.createdAt = createdAt;
+        this.types = types;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public ArrayList<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(ArrayList<String> types) {
         this.types = types;
     }
 
