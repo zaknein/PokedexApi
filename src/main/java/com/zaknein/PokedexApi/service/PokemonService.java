d5b90bf96cc527c1ee8d3b014f0efc29d85532a1
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-20 12:42:28 -0300

    create pokemonService

---
+++ src/main/java/com/zaknein/PokedexApi/service/PokemonService.java
@@ -0,0 +1,15 @@
+package com.zaknein.PokedexApi.service;
+
+
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+
+
+
+@Service
+public class PokemonService{
+
+    private Map<Integer,Pokemon> pokemonMap = new HashMap<>();
+
+
+}
