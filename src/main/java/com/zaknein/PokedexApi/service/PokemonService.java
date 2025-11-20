d289c30b3a8f66375cf41c9494216d075ebb31a7
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-20 12:51:45 -0300

    createPokemon methd created

--- src/main/java/com/zaknein/PokedexApi/service/PokemonService.java
+++ src/main/java/com/zaknein/PokedexApi/service/PokemonService.java
@@ -11,5 +11,27 @@ public class PokemonService{
 
     private Map<Integer,Pokemon> pokemonMap = new HashMap<>();
 
+    private int futurePokeId = 1;
+
+    @Override
+    public Pokemon createPokemon(PokemonCreater pokemonC ){
+
+        int pokemonId = futurePkmnId++;
+
+        Pokemon newPoke = new Pokemon(pokemonId, PokemonC.getName, PokemonC.getSpecies, pokemonC.getHeight,
+         pokemonC.getWeight, pokemonC.getDescription, pokemonC.getCreatedAt, pokemonC.getTypes)
+
+        pokemonMap.put(pokemonId, newPoke);
+
+    }
+
+
+
+
+
+
+
+
+
 
 }
