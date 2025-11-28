1c989c9055b9052b3b62a71094c5c2c7a6c74b51
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-11-28 20:36:52 -0300

    capturedPokemon controller
    captured pokemons & get endpoint created empty

---
+++ src/main/java/com/zaknein/PokedexApi/controller/UserCaptredPokemonController.java
@@ -0,0 +1,25 @@
+package com.zaknein.PokedexApi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zaknein.PokedexApi.domain.Pokemon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+@RestController
+@CrossOrigin
+public class UserCaptredPokemonController {
+
+    // @GetMapping("/users/{userId}/pokemons")
+    // public Pokemon getCapturedPokemon(@PathVariable int id) {
+    //     return 
+    // }
+        
+
+    
+}
