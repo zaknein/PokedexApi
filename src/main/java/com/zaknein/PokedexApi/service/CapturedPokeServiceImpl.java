803638b94401d398cdbd9182fe2b66ca7db46582
German Kröger <116314842+zaknein@users.noreply.github.com>
2025-12-04 10:08:15 -0300

    CaapturedPokeMap

--- src/main/java/com/zaknein/PokedexApi/service/CapturedPokeServiceImpl.java
+++ src/main/java/com/zaknein/PokedexApi/service/CapturedPokeServiceImpl.java
@@ -1,11 +1,20 @@
 package com.zaknein.PokedexApi.service;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.zaknein.PokedexApi.domain.CapturedPokemon;
 
+
+
 public class CapturedPokeServiceImpl implements CapturedPokeService {
 
+    private Map<Integer,CapturedPokemon> CaapturedPokeMap = new Hasmap<>();
+
+    private int CapPokeId = 1;
+
+    
     @Override
     public CapturedPokemon enterCapturedPoke() {
         // TODO Auto-generated method stub
